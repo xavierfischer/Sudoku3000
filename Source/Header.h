ebe0cb1ce1f14ecb308e67110afcfcbb433a16af
antoineb19 <antoinebenoit@msn.com>
2015-12-05 21:28:43 +0100

    Add header

---
+++ Source/Header.h
@@ -0,0 +1,13 @@
+
+
+#ifndef Sudoku_Header_h
+#define Sudoku_Header_h
+
+#include <iostream>
+#include <string>
+#include <list>
+#include <exception>
+#include "Cell.h"
+using namespace std;
+
+#endif
