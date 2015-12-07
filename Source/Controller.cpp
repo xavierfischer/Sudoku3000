c74317d19de1c76ea0ea5774c35541c4b5fa90c6
Maxime Piard <maxime.piard@student.ecp.fr>
2015-12-07 10:07:36 +0100

    Bouttons jolis
    Signed-off-by: Maxime Piard <maxime.piard@student.ecp.fr>

--- Source/Controller.cpp
+++ Source/Controller.cpp
@@ -472,7 +472,7 @@ void Controller::run(){
 					}
 					// ButtonVal
 					else {
-						for (int i = 1; i <= 9; ++i) {
+						for (int i = 0; i <= 9; ++i) {
 							if (ButtonVal[i].getGlobalBounds().contains((float)Position.x, (float)Position.y) & ActiveCell[0]==1) {
 								ButtonVal[i].CallHandler();
 							}
