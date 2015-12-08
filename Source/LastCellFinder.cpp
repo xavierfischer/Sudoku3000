72239ac091306a2c7eba3d25480a450e2c699052
xavierfischer <xavier.fischer@student.ecp.fr>
2015-12-08 23:12:05 +0100

    Update LastCellFinder.cpp

--- Source/LastCellFinder.cpp
+++ Source/LastCellFinder.cpp
@@ -6,45 +6,37 @@
 
 LastCellFinder::LastCellFinder(NineUplet n) :nineUplet(n) {}
 
-/*
-	Cette classe complète si possible le nineUple donné et renvoie true si une modification a eu lieu
-*/
+
 
 bool LastCellFinder::fill() {
-	//nineUplet.getCells();
+	nineUplet.getCells();
 	if (nineUplet.isConsistent()) {
 		for (int i = 0; i < 9; i++) {
-			//int v = nineUplet.getCells()[i].value;
+	int v = nineUplet.getCells()[i].value;
 		}
 	}
 	return false;
-	//}
-
-	//a = nineUplet.getCells
 
 
+	a = nineUplet.getCells
 
-		int count = 0;
-	int count2 = 0;
-	int pos = 0;
 
-	/*
+
+
+	
 	//On compte le nombre de cellule vide
 	for (int i = 0; i <= 8; i++) {
 		if (tableCells[i].isEmpty()) {
-			count++;
 			pos = i;//On enregistre la position de la derniere cellule vide parcourue (la seule dans le cas qui nous interesse)
 		}
 	}
 	if (count == 1) {//Si on a effectivement un changement à faire (si il n'y a qu'une seule cellule vide)
 		for (int i = 1; i <= 9; i++) {//Pour chaque nombre en 1 et 9
-			count2 = 0;
 			for (int j = 0; j <= 8; j++) {//On compte le nombre de fois qu'il apparait
-				if (tableCells[j].numberCell == i) {
-					count2++;
-				}
+				if (tableCells[j].numberCell == i) ;
+				
 			}//Si le nombre n'apparait pas
-			if (count2 == 0) {
+                               else
 
 			}
 		}
